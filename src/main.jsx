b3294b6e2dc6dd0accd16976a05c8187f53d193e
tianzexiang <tianzexiang@wps.cn>
2024-09-29 17:47:19 +0800

    feat: 渲染虚拟dom树

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
 import './index.css'
+import { createRoot, createElement } from './mini-react/react-dom/index.js'  
+
+const element = createElement('h1', { id: 'title' }, 'Hello World')
+createRoot(document.getElementById('root')).render(element)
 
 
-createRoot(document.getElementById('root')).render(<App />)
+
